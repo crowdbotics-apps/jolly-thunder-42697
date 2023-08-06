a3b3cf24c89478e50c15d6b21f006c7fe79f5a63
Crowdbotics <team@crowdbotics.com>
2023-08-06 20:31:23 +0000

    Initial commit.

---
+++ backend/jolly_thunder_42697/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jolly_thunder_42697 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jolly_thunder_42697.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "jolly-thunder-42697",
+  url: "https://jolly-thunder-42697.botics.co",
+  api: "https://jolly-thunder-42697.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
